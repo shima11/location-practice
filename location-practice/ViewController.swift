c8d0dd3a528c7bf898d82148817ba9f358df51b1
Shima <shima.jin@icloud.com>
2019-05-30 21:53:57 +0900

    Initial Commit

---
+++ location-practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  location-practice
+//
+//  Created by jinsei_shima on 2019/05/30.
+//  Copyright © 2019 jinsei_shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
